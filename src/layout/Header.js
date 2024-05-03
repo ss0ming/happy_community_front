74f1be7ea8c16e99803f192bdd7e2a2856629d03
euniiiii <hee9920@naver.com>
2024-05-03 11:45:41 +0900

    헤더 추가

---
+++ src/layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="title">아무말 대잔치</div>
+        </header>
+    )
+}
+
+export default Header;
