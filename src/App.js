0fb1cde3dd1b5dbdc43d1a9941fb18780b379226
ss0ming <hee9920@naver.com>
2024-09-16 15:14:30 +0900

    feat: 배경 디자인 및 헤더 수정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import root from "./router/root";
 import GlobalStyle from './styles/GlobalStyle';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
     <>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#DBD2D2',
+          defaultActiveBg: '#FFFFFF'
+        },
+      }}
+    >
       <GlobalStyle/>
-      <RouterProvider router={root}/>
+      <RouterProvider style={{backgroundImage: ''}}router={root}/>
+      </ConfigProvider>
     </>    
   );
 }
